84f3bcd462a4d77e1bf74c0dfec2c36fe5996fa6
balle <ballesteros.1mm@gmail.com>
2024-03-13 20:50:38 -0500

    tercer commit

---
+++ operaciones.js
@@ -0,0 +1,35 @@
+console.log("operaciones...")
+
+let counter = 1000;
+console.log(counter)
+//suma
+counter = counter + 20; 1020
+console.log(counter)
+counter += 30; // 1050
+console.log(counter)
+//resta
+counter = counter - 20; // 1030
+console.log(counter)
+counter -= 30; // 1000
+console.log(counter)
+//multiplicacion 
+counter = counter *3; // 3000
+console.log(counter)
+counter *= 4; // 12000
+console.log(counter)
+//division 
+counter = counter / 5; // 2400
+console.log(counter)
+counter /= 4; // 600
+console.log(counter)
+//residuo 
+counter = counter % 3; // 0
+console.log(counter)
+counter %= 3; // 0
+console.log(counter)
+// exponencial 
+counter = counter **3; // 0
+console.log(counter)
+counter **= 2; // 0
+console.log(counter)
+
