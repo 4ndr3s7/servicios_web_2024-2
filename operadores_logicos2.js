648baca23b76bb14fde03cb77aaef87fa227ea17
balle <ballesteros.1mm@gmail.com>
2024-03-20 21:00:51 -0500

    x

--- operadores_logicos2.js
+++ operadores_logicos2.js
@@ -19,6 +19,10 @@ console.log(!false); //  Verdad
 console.log(!true); //   Falsa      
 console.log(!true); //   Falsa
 
+
+let counter = 10;
+consele.log
+
 if (counter > 100) {
     console.log(`el valor del  counter es: ${counter} es mayor que 100 `);
     console.log(`el valor del  counter es: ${counter} es menor que 100 `);   
